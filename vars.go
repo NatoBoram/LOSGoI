6bf697accbfe73a7d76830ddb8e381ff35fb2a72
Nato Boram <nato.boram@gmail.com>
2019-03-08 00:07:45 -0500

    error errHashEmptyFolder

--- vars.go
+++ vars.go
@@ -1,5 +1,13 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"errors"
+)
 
 var db *sql.DB
+
+// Errors
+var (
+	errHashEmptyFolder = errors.New("this is the hash of an empty folder")
+)
