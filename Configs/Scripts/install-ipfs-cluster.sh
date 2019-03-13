448f34e576ec26002f3630177456a69000af2e18
Nato Boram <nato.boram@gmail.com>
2019-03-13 12:03:01 -0400

    added some dependencies

--- Configs/Scripts/install-ipfs-cluster.sh
+++ Configs/Scripts/install-ipfs-cluster.sh
@@ -3,9 +3,25 @@
 FROM=`pwd`
 
 rm -rf ~/go/src/github.com/ipfs/ipfs-cluster
-go get -u -v -fix github.com/ipfs/ipfs-cluster
-systemctl --user stop ipfs-cluster
+GO111MODULE=on go get -u -v -fix github.com/ipfs/ipfs-cluster
 
+# Dependencies
+go get -u -v -fix github.com/gorilla/mux
+go get -u -v -fix github.com/hashicorp/raft
+go get -u -v -fix github.com/hashicorp/raft-boltdb
+go get -u -v -fix github.com/hsanjuan/go-libp2p-gostream
+go get -u -v -fix github.com/hsanjuan/go-libp2p-http
+go get -u -v -fix github.com/ipfs/go-fs-lock
+go get -u -v -fix github.com/libp2p/go-libp2p-consensus
+go get -u -v -fix github.com/libp2p/go-libp2p-pubsub
+go get -u -v -fix github.com/libp2p/go-libp2p-raft
+go get -u -v -fix github.com/rs/cors
+go get -u -v -fix go.opencensus.io/vendor/google.golang.org/api/support/bundler
+go get -u -v -fix google.golang.org/api/support/bundler
+go get -u -v -fix google.golang.org/genproto/googleapis/rpc/status
+
+# Make Install
+systemctl --user stop ipfs-cluster
 cd ~/go/src/github.com/ipfs/ipfs-cluster
 make install
 systemctl --user start ipfs-cluster
