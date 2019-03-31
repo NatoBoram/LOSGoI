aeb228067f718c05b51ca00085f130f0815d41ea
Nato Boram <nato.boram@gmail.com>
2019-03-31 00:15:40 -0400

    some more go get in a script

--- Configs/Scripts/install-ipfs-cluster.sh
+++ Configs/Scripts/install-ipfs-cluster.sh
@@ -11,6 +11,7 @@ rm -rf ~/go/src/github.com/ipfs/ipfs-cluster
 go get -u -v -fix github.com/ipfs/ipfs-cluster
 
 # Dependencies
+go get -u -v -fix github.com/apache/thrift/lib/go/thrift
 go get -u -v -fix github.com/gorilla/mux
 go get -u -v -fix github.com/hashicorp/raft
 go get -u -v -fix github.com/hashicorp/raft-boltdb
@@ -20,13 +21,13 @@ go get -u -v -fix github.com/ipfs/go-fs-lock
 go get -u -v -fix github.com/libp2p/go-libp2p-consensus
 go get -u -v -fix github.com/libp2p/go-libp2p-pubsub
 go get -u -v -fix github.com/libp2p/go-libp2p-raft
+go get -u -v -fix github.com/prometheus/client_golang/prometheus
+go get -u -v -fix github.com/prometheus/client_golang/prometheus/promhttp
 go get -u -v -fix github.com/rs/cors
+go get -u -v -fix github.com/zenground0/go-dot
 go get -u -v -fix go.opencensus.io/vendor/google.golang.org/api/support/bundler
 go get -u -v -fix google.golang.org/api/support/bundler
 go get -u -v -fix google.golang.org/genproto/googleapis/rpc/status
-go get -u -v -fix github.com/zenground0/go-dot
-go get -u -v -fix github.com/prometheus/client_golang/prometheus
-go get -u -v -fix github.com/prometheus/client_golang/prometheus/promhttp
 
 # SystemCtl Stop
 systemctl --user stop ipfs-cluster
