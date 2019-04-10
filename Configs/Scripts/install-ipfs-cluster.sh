51a3b71ce4bc52e074dcaf97e27db3767a96d852
Nato Boram <nato.boram@gmail.com>
2019-04-10 00:14:23 -0400

    GO111MODULE=on

--- Configs/Scripts/install-ipfs-cluster.sh
+++ Configs/Scripts/install-ipfs-cluster.sh
@@ -4,34 +4,19 @@
 cd
 
 # Go Get - IPFS
-go get -u -v -fix github.com/ipfs/go-ipfs
+GO111MODULE=on go get -u -v -fix github.com/ipfs/go-ipfs
 
 # Go Get - IPFS Cluster
-rm -rf ~/go/src/github.com/ipfs/ipfs-cluster
-go get -u -v -fix github.com/ipfs/ipfs-cluster
-
-# Dependencies
-go get -u -v -fix github.com/apache/thrift/lib/go/thrift
-go get -u -v -fix github.com/gorilla/mux
-go get -u -v -fix github.com/hashicorp/raft
-go get -u -v -fix github.com/hashicorp/raft-boltdb
-go get -u -v -fix github.com/hsanjuan/go-libp2p-gostream
-go get -u -v -fix github.com/hsanjuan/go-libp2p-http
-go get -u -v -fix github.com/ipfs/go-fs-lock
-go get -u -v -fix github.com/libp2p/go-libp2p-consensus
-go get -u -v -fix github.com/libp2p/go-libp2p-pubsub
-go get -u -v -fix github.com/libp2p/go-libp2p-raft
-go get -u -v -fix github.com/prometheus/client_golang/prometheus
-go get -u -v -fix github.com/prometheus/client_golang/prometheus/promhttp
-go get -u -v -fix github.com/rs/cors
-go get -u -v -fix github.com/zenground0/go-dot
-go get -u -v -fix go.opencensus.io/vendor/google.golang.org/api/support/bundler
-go get -u -v -fix google.golang.org/api/support/bundler
-go get -u -v -fix google.golang.org/genproto/googleapis/rpc/status
+GO111MODULE=on go get -u -v -fix github.com/ipfs/ipfs-cluster
 
 # SystemCtl Stop
 systemctl --user stop ipfs-cluster
 systemctl --user stop ipfs
+
+# Cleanup State
+#ipfs-cluster-service state cleanup
+#y
+rm -rf ~/.ipfs-cluster/raft.old.*
 
 # Make Install - IPFS
 cd ~/go/src/github.com/ipfs/go-ipfs
