85321c67e1156e7e1a427d8a110a5826d07e9138
Nato Boram <nato.boram@gmail.com>
2019-04-28 00:41:44 -0400

    better scripts

---
+++ Configs/Scripts/cleanup_ipfs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Stop
+systemctl --user stop ipfs-cluster
+systemctl --user stop ipfs
+
+# Cleanup IPFS Cluster
+ipfs-cluster-service state cleanup
+y
+rm -rf ~/.ipfs-cluster/raft.old.*
+
+# Cleanup IPFS
+ipfs pin ls -q --type recursive | xargs ipfs pin rm
+
+# Start
+systemctl --user start ipfs
+systemctl --user start ipfs-cluster
