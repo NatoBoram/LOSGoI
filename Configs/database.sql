f123a558a063833d9f301a92c33b39a5b49de4f1
Nato Boram <nato.boram@gmail.com>
2019-07-16 23:25:07 -0400

    changed database views

--- Configs/database.sql
+++ Configs/database.sql
@@ -1,44 +1,56 @@
-USE `LOSGoI`;
+use `LOSGoI`;
 -- Builds
-DROP TABLE IF EXISTS `builds`;
-CREATE TABLE IF NOT EXISTS `builds`(
-    `device` VARCHAR(16) NOT NULL,
-    `date` DATE NOT NULL,
-    `datetime` DATETIME NOT NULL,
-    `filename` VARCHAR(64) NOT NULL,
-    `filepath` VARCHAR(128) NOT NULL,
-    `sha1` VARCHAR(40) NOT NULL,
-    `sha256` VARCHAR(64) NOT NULL,
-    `size` INTEGER NOT NULL,
-    `type` VARCHAR(16) NOT NULL,
-    `version` VARCHAR(8) NOT NULL,
-    `ipfs` VARCHAR(128) NOT NULL PRIMARY KEY
+drop table if exists `builds`;
+create table if not exists `builds`(
+  `device` varchar(16) not null,
+  `date` date not null,
+  `datetime` datetime not null,
+  `filename` varchar(64) not null,
+  `filepath` varchar(128) not null,
+  `sha1` varchar(40) not null,
+  `sha256` varchar(64) not null,
+  `size` INTEGER not null,
+  `type` varchar(16) not null,
+  `version` varchar(8) not null,
+  `ipfs` varchar(128) not null primary key
 );
+-- Max
+create
+or replace view `builds_max` as
+select
+  `device`,
+  max(`datetime`) as `datetime`
+from
+  `builds`
+group by
+  `device`;
 -- Latest Builds
-DROP VIEW IF EXISTS `builds_latest`;
-CREATE VIEW `builds_latest` AS
-SELECT
-    `device`,
-    MAX(`date`) `date`,
-    `datetime`,
-    `filename`,
-    `filepath`,
-    `sha1`,
-    `sha256`,
-    `size`,
-    `type`,
-    `version`,
-    `ipfs`
-FROM
-    `builds`
-GROUP BY
-    `device`;
+  create
+  or replace view `builds_latest` as
+select
+  `builds`.`device`,
+  `builds`.`date`,
+  `builds`.`datetime`,
+  `builds`.`filename`,
+  `builds`.`filepath`,
+  `builds`.`sha1`,
+  `builds`.`sha256`,
+  `builds`.`size`,
+  `builds`.`type`,
+  `builds`.`version`,
+  `builds`.`ipfs`
+from
+  `builds`
+  join `builds_max`
+where
+  `builds`.`device` = `builds_max`.`device`
+  and `builds`.`datetime` = `builds_max`.`datetime`;
 -- Remove garbage from builds
-UPDATE
-    `builds`
-SET
-    `ipfs` = REPLACE (ipfs, CHAR(13), '');
-UPDATE
-    `builds`
-SET
-    `ipfs` = REPLACE (ipfs, CHAR(10), '');
+update
+  `builds`
+set
+  `ipfs` = replace(ipfs, char(13), '');
+update
+  `builds`
+set
+  `ipfs` = replace(ipfs, char(10), '');
