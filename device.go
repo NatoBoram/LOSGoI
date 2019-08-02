732b608eb6e2c70a57959f694ca428b8068e0436
Nato Boram <nato.boram@gmail.com>
2019-08-01 23:37:52 -0400

    trim only keeps the latest build

--- device.go
+++ device.go
@@ -30,21 +30,43 @@ func (devices *Devices) Count() (count int) {
 	return
 }
 
-// Trim trims devices that were already hashed
+// Trim trims devices that were already hashed and those that aren't the latest of the device.
 func (devices *Devices) Trim() (builds []*Build) {
-	for _, device := range *devices {
-		for _, build := range device {
 
-			// Check if it needs to be hashed.
-			bh, err := build.Select()
-			if err == sql.ErrNoRows {
-				builds = append(builds, build)
-			} else if err != nil {
-				fmt.Println("Couldn't select build", aurora.Green(build.Filename).String()+".")
-				fmt.Println(err.Error())
-			} else {
-				fmt.Println("Skipping", aurora.Green(bh.Build.Filename).String()+".")
+	// Select only the newest build by device
+	var newestBuilds []*Build
+	for _, device := range *devices {
+		var latest time.Time
+
+		// Get the max date for this device
+		for _, build := range device {
+			if latest.Before(build.Datetime.Time) {
+				latest = build.Datetime.Time
 			}
+		}
+
+		// Put the build corresponding to the max date in the array
+		for _, build := range device {
+			if latest.Equal(build.Datetime.Time) {
+				newestBuilds = append(newestBuilds, build)
+			} else {
+				fmt.Println("Skipping", aurora.Green(build.Filename).String()+".")
+			}
+		}
+	}
+
+	// Compare with the database
+	for _, build := range newestBuilds {
+
+		// Check if it needs to be hashed.
+		bh, err := build.Select()
+		if err == sql.ErrNoRows {
+			builds = append(builds, build)
+		} else if err != nil {
+			fmt.Println("Couldn't select build", aurora.Green(build.Filename).String()+".")
+			fmt.Println(err.Error())
+		} else {
+			fmt.Println("Skipping", aurora.Green(bh.Build.Filename).String()+".")
 		}
 	}
 
