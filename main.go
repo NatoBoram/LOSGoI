e67eef61bf26675240034664dec56b394869b314
Nato Boram <nato.boram@gmail.com>
2020-04-20 19:21:24 +0000

    Editor Config

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	// License
 	fmt.Println("")
 	fmt.Println(aurora.Bold("LOSGoI :"), "LineageOS Goes to IPFS.")
-	fmt.Println("Copyright © 2019 Nato Boram")
+	fmt.Println("Copyright © 2018-2020 Nato Boram")
 	fmt.Println("This program is free software : you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY ; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.")
 	fmt.Println(aurora.Bold("Contact :"), aurora.Blue("https://gitlab.com/NatoBoram/LOSGoI"))
 	fmt.Println("")
--- pkg/lineageos/recovery.go
+++ pkg/lineageos/recovery.go
@@ -4,7 +4,7 @@ package lineageos
 type Recovery struct {
 	Filename string `json:"filename"`
 	Filepath string `json:"filepath"`
-	Sha1     string `json:"sha1"`    
-	Sha256   string `json:"sha256"`  
-	Size     int64  `json:"size"`    
+	Sha1     string `json:"sha1"`
+	Sha256   string `json:"sha256"`
+	Size     int64  `json:"size"`
 }
